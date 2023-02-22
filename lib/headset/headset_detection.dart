0fafac9dfff4b7cf381e9d720f5d9ef9ed2eb687
Anikshakya <aniklinkin@gmail.com>
2023-02-22 15:37:09 +0545

    [Added:: background app handler than handels code when the app is in the back ground]

--- lib/headset/headset_detection.dart
+++ lib/headset/headset_detection.dart
@@ -1,6 +1,10 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:headphonetest/main.dart';
 import 'package:headset_connection_event/headset_event.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 
 class HeadsetDetection extends StatefulWidget {
@@ -11,6 +15,124 @@ class HeadsetDetection extends StatefulWidget {
 }
 
 class _HeadsetDetectionState extends State<HeadsetDetection> with WidgetsBindingObserver {
+
+  ReceivePort? _receivePort;
+
+  void _initForegroundTask() {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'notification_channel_id',
+        channelName: 'Foreground Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+        iconData: const NotificationIconData(
+          resType: ResourceType.mipmap,
+          resPrefix: ResourcePrefix.ic,
+          name: 'launcher',
+          backgroundColor: Colors.orange,
+        ),
+        buttons: [
+          const NotificationButton(id: 'sendButton', text: 'Send'),
+          const NotificationButton(id: 'testButton', text: 'Test'),
+        ],
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        interval: 5000,
+        isOnceEvent: false,
+        autoRunOnBoot: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
+    );
+  }
+
+  Future<bool> _startForegroundTask() async {
+    // "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted for
+    // onNotificationPressed function to be called.
+    //
+    // When the notification is pressed while permission is denied,
+    // the onNotificationPressed function is not called and the app opens.
+    //
+    // If you do not use the onNotificationPressed or launchApp function,
+    // you do not need to write this code.
+    if (!await FlutterForegroundTask.canDrawOverlays) {
+      final isGranted =
+          await FlutterForegroundTask.openSystemAlertWindowSettings();
+      if (!isGranted) {
+        debugPrint('SYSTEM_ALERT_WINDOW permission denied!');
+        return false;
+      }
+    }
+
+    // You can save data using the saveData function.
+    await FlutterForegroundTask.saveData(key: 'customData', value: 'hello');
+
+    // Register the receivePort before starting the service.
+    final ReceivePort? receivePort = FlutterForegroundTask.receivePort;
+    final bool isRegistered = _registerReceivePort(receivePort);
+    if (!isRegistered) {
+      debugPrint('Failed to register receivePort!');
+      return false;
+    }
+
+    if (await FlutterForegroundTask.isRunningService) {
+      return FlutterForegroundTask.restartService();
+    } else {
+      return FlutterForegroundTask.startService(
+        notificationTitle: 'Foreground Service is running',
+        notificationText: 'Tap to return to the app',
+        callback: startCallback,
+      );
+    }
+  }
+
+  // Future<bool> _stopForegroundTask() {
+  //   return FlutterForegroundTask.stopService();
+  // }
+
+  bool _registerReceivePort(ReceivePort? newReceivePort) {
+    if (newReceivePort == null) {
+      return false;
+    }
+
+    _closeReceivePort();
+
+    _receivePort = newReceivePort;
+    _receivePort?.listen((message) {
+      if (message is int) {
+        debugPrint('eventCount: $message');
+      } else if (message is String) {
+        if (message == 'onNotificationPressed') {
+          Navigator.of(context).pushNamed('/resume-route');
+        }
+      } else if (message is DateTime) {
+        debugPrint('timestamp: ${message.toString()}');
+      }
+    });
+
+    return _receivePort != null;
+  }
+
+  void _closeReceivePort() {
+    _receivePort?.close();
+    _receivePort = null;
+  }
+
+  T? _ambiguate<T>(T? value) => value;
+
+  @override
+  void dispose() {
+    _closeReceivePort();
+    super.dispose();
+  }
+
+
 
   final headsetPlugin = HeadsetEvent();
   HeadsetState? _headsetState;
@@ -38,31 +160,44 @@ class _HeadsetDetectionState extends State<HeadsetDetection> with WidgetsBinding
           : callNumber();
       });
     });
+
+    _initForegroundTask();
+    _ambiguate(WidgetsBinding.instance)?.addPostFrameCallback((_) async {
+      // You can get the previous ReceivePort without restarting the service.
+      if (await FlutterForegroundTask.isRunningService) {
+        final newReceivePort = FlutterForegroundTask.receivePort;
+        _registerReceivePort(newReceivePort);
+      }
+    });
+
+    _startForegroundTask();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.headset,
-              color: _headsetState == HeadsetState.CONNECT
-                  ? Colors.green
-                  : Colors.red,
-            ),
-            Text('State : $_headsetState\n'),
-            const SizedBox(height: 20,),
-            InkWell(
-              onTap: (){
-                callNumber();
-              },
-              child: const Icon(Icons.phone)
-            ),
-          ],
+    return WithForegroundTask(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.headset,
+                color: _headsetState == HeadsetState.CONNECT
+                    ? Colors.green
+                    : Colors.red,
+              ),
+              Text('State : $_headsetState\n'),
+              const SizedBox(height: 20,),
+              InkWell(
+                onTap: (){
+                  callNumber();
+                },
+                child: const Icon(Icons.phone)
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,119 @@
+import 'dart:isolate';
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:headphonetest/headset/headset_detection.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// The callback function should always be a top-level function.
+@pragma('vm:entry-point')
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(MyTaskHandler());
+}
+class MyTaskHandler extends TaskHandler {
+  SendPort? _sendPort;
+  int _eventCount = 0;
+
+  @override
+  Future<void> onStart(DateTime timestamp, SendPort? sendPort) async {
+    _sendPort = sendPort;
+
+    // You can use the getData function to get the stored data.
+    final customData =
+        await FlutterForegroundTask.getData<String>(key: 'customData');
+    debugPrint('customData: $customData');
+  }
+
+  @override
+  Future<void> onEvent(DateTime timestamp, SendPort? sendPort) async {
+    FlutterForegroundTask.updateService(
+      notificationTitle: 'MyTaskHandler',
+      notificationText: 'eventCount: $_eventCount',
+    );
+
+    // Send data to the main isolate.
+    sendPort?.send(_eventCount);
+
+    _eventCount++;
+  }
+
+  @override
+  Future<void> onDestroy(DateTime timestamp, SendPort? sendPort) async {
+    // You can use the clearAllData function to clear all the stored data.
+    await FlutterForegroundTask.clearAllData();
+  }
+
+  @override
+  void onButtonPressed(String id) {
+    // Called when the notification button on the Android platform is pressed.
+    debugPrint('onButtonPressed >> $id');
+  }
+
+  @override
+  void onNotificationPressed() {
+    // Called when the notification itself on the Android platform is pressed.
+    //
+    // "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted for
+    // this function to be called.
+
+    // Note that the app will only route to "/resume-route" when it is exited so
+    // it will usually be necessary to send a message through the send port to
+    // signal it to restore state when the app is already started.
+    FlutterForegroundTask.launchApp("/resume-route");
+    _sendPort?.send('onNotificationPressed');
+  }
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  void _initForegroundTask() {
+  FlutterForegroundTask.init(
+    androidNotificationOptions: AndroidNotificationOptions(
+      channelId: 'notification_channel_id',
+      channelName: 'Foreground Notification',
+      channelDescription: 'This notification appears when the foreground service is running.',
+      channelImportance: NotificationChannelImportance.LOW,
+      priority: NotificationPriority.LOW,
+      iconData: const NotificationIconData(
+        resType: ResourceType.mipmap,
+        resPrefix: ResourcePrefix.ic,
+        name: 'launcher',
+      ),
+      buttons: [
+        const NotificationButton(id: 'sendButton', text: 'Send'),
+        const NotificationButton(id: 'testButton', text: 'Test'),
+      ],
+    ),
+    iosNotificationOptions: const IOSNotificationOptions(
+      showNotification: true,
+      playSound: false,
+    ),
+    foregroundTaskOptions: const ForegroundTaskOptions(
+      interval: 5000,
+      isOnceEvent: false,
+      autoRunOnBoot: true,
+      allowWakeLock: true,
+      allowWifiLock: true,
+    ),
+  );
+}
+
+@override
+void initState() {
+  super.initState();
+  _initForegroundTask();
+}
+
 
   // This widget is the root of your application.
   @override
